887b06f1f9a5f20cb7663b25480aa936bb2031a9
Poojashree Basa <poojashree.basa3@gmail.com>
2026-02-23 10:03:11 +0530

    Add in remaining files

---
+++ Lessons/1.10/1.10_Data_Modeling.sql
@@ -0,0 +1,15 @@
+SELECT
+    job_id,
+    job_title_short,
+    salary_year_avg,
+    company_id
+FROM
+    job_postings_fact
+LIMIT 10;
+
+
+SELECT *
+FROM information_schema.table_constraints
+WHERE table_catalog = 'data_jobs';
+
+PRAGMA show_tables_expanded;
